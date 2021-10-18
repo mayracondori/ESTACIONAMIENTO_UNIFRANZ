196454cea1d193305df7043d10b0f5f3e40324e3
Nayelicopa16 <78456339+Nayelicopa16@users.noreply.github.com>
2021-10-18 19:11:37 -0400

    Update PlantillaController.php

--- app/Http/Controllers/PlantillaController.php
+++ app/Http/Controllers/PlantillaController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
+
 
 class PlantillaController extends Controller
 {
-    public function plantilla(){
+    
+    public function plantillaadmin(){
         //metodo inicial
-        return view('layouts.plantilla');
+        return view('layouts.plantillaadmin');
     }
-
-   
+    public function plantillausuario(){
+        //metodo inicial
+        return view('layouts.plantillausuario');
+    }
+    
+    public function cerrarsesion(){
+       
+        session_abort();
+        session_unset();
+        
+        return redirect()->route('index');
+        }
 
 }
