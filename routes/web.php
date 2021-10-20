29a6c1317e37a4c2bcaf41fdd9cd2a8bf8660f79
mayracondori <64332144+mayracondori@users.noreply.github.com>
2021-10-20 15:23:48 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,29 @@ use App\Http\Controllers\PlantillaController;
 
 Route::get('/', HomeController::class,'index')->name('index');
 
+Route::get('login', [PlantillaController::class,'login'])->name('login');
+Route::post('/loginme', [PlantillaController::class,'loginme'])->name('/loginme');
 Route::get('layouts', [PlantillaController::class, 'index'])->name('layouts.index');
 Route::get('layouts/plantilla', [PlantillaController::class, 'plantilla']);
 Route::get('layouts/plantillainicio', [PlantillaController::class, 'plantillainicio']);
 
-Route::get('layouts/plantillaadmin', [PlantillaController::class, 'plantillaadmin']);
+Route::get('layouts/plantillausu', [PlantillaController::class, 'plantillausu']);
 Route::get('layouts/plantillausuario', [PlantillaController::class, 'plantillausuario']);
 Route::get('layouts/cerrarsesion', [PlantillaController::class, 'cerrarsesion'])->name('layouts/cerrarsesion');
 
 
-Route::get('usuario', [UsuarioController::class, 'index'])->name('index');
+Route::get('usuario', [UsuarioController::class, 'index'])->name('usuario.index');
 Route::get('usuario/usuarios', [UsuarioController::class, 'listausuarios'])->name('usuario.usuarios');
 Route::get('usuario/horarios', [UsuarioController::class, 'listahorarios']);
 Route::get('usuario/registro', [UsuarioController::class, 'registro'])->name('registro');
 Route::post('usuario/completaregistro', [UsuarioController::class, 'completaregistro'])->name('completaregistro');
 Route::POST('usuario/modifpreguntas', [UsuarioController::class, 'modifpreguntas'])->name('modifpreguntas');
 
-Route::get('admin', [AdminController::class, 'index'])->name('admin.index');
+Route::get('admin/inicio', [AdminController::class, 'index'])->name('admin.index');
+Route::get('admin/nuevareserva', [AdminController::class, 'nuevareserva'])->name('admin.nuevareserva');
+Route::get('admin/misreservas', [AdminController::class, 'misreservas'])->name('admin.misreservas');
+Route::POST('admin/completareserva', [AdminController::class, 'completareserva'])->name('completareserva');
+Route::POST('admin/mireserva', [AdminController::class, 'mireserva'])->name('mireserva');
+Route::get('admin/normas', [AdminController::class, 'normas'])->name('admin.normas');
+
+
