d10dff97dd858f18bb3eed830acb043377c7fd48
mayracondori <64332144+mayracondori@users.noreply.github.com>
2021-10-20 15:26:05 -0400

    Add files via upload

---
+++ app/Models/Reserva.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    use HasFactory;
+
+    //direcciona a la tabla de la base de datos
+    protected $table= "reserva";
+
+}
+
